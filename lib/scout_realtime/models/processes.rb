99b66662007105d8ecaf539f16459659e79a90b8
Mr. Matt <matt@composition9.com>
2014-01-15 17:16:16 -0700

    process charts in js rather than ruby

--- lib/scout_realtime/models/processes.rb
+++ lib/scout_realtime/models/processes.rb
@@ -10,16 +10,4 @@ class Scout::Realtime::Processes < Scout::Realtime::Metric
     @collector = ServerMetrics::Processes.new()
     super
   end
-
-  def latest_run
-    top_memory_processes = get_top_processes(@latest_run, :memory, 10)
-    top_cpu_processes = get_top_processes(@latest_run, :cpu, 10)
-
-    @latest_run.select { |cmd, values| (top_memory_processes + top_cpu_processes).include?(cmd) }
-  end
-  
-  private
-    def get_top_processes(data, order_by, num)
-      data.values.sort { |a, b| a[order_by] <=> b[order_by] }.reverse[0...num].map { |values| values[:cmd] }
-    end
 end
